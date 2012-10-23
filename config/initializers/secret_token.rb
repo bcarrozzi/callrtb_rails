d871545078787fae9dbc38ce7a183ed623f38b1b
Brian Carrozzi <bcarrozzi@lukainc.com>
2012-10-23 14:43:01 -0700

    created callrtb_ruby repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Callrtb::Application.config.secret_token = '9f48b40a014ecaa249f926d0109cb49de8d039b117a4503ff0694d6d910fa4f6b6fd8cae208a6ea0d166b71333df8273fa477d63b47507a0aaa2d813d617093a'
